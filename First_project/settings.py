23f9caba664626c5191175254df05f08f7ea57d0
sanju <sk079997@gmail.com>
2021-06-03 23:31:32 +0530

    third3 Commit

--- First_project/settings.py
+++ First_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -127,8 +127,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
+    BASE_DIR / "staticfiles",
+    '/var/www/staticfiles/',
 ]
 
 django_heroku.settings(locals())
